dc1c3cbad5b1160c91616fea7b433d2cfbef694a
Frank Calise <fcalise@gmail.com>
2019-03-29 23:24:02 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# Frank Calise personal website
+
+This was bootstrapped via
+[egghead-gatsby-starter](https://github.com/eggheadio/gatsby-starter-egghead-blog)
+
+This is based on Robin Wieruch's
+https://github.com/rwieruch/gatsby-mdx-blog-starter-project
+
+## Features
+
+* MDX: JavaScript/React in Markdown
+  * see */mdx-example* blog post
+* Prism.js: Syntax Highlighting
+* Pagination
+* Categories
+* Styled Components
+
+## [➞ Demo](https://egghead-gatsby-starter.netlify.com/)
+
+## Setup
+
+- `git clone git@github.com:eggheadio/gatsby-starter-egghead-blog.git`
+- `cd gatsby-starter-egghead-blog`
+- `npm install`
+- `gatsby develop`
+- visit http://localhost:8000
+
+## Setup via Gatsby CLI
+
+- `gatsby new gatsby-starter-egghead-blog git@github.com:eggheadio/gatsby-starter-egghead-blog.git`
+- `cd gatsby-starter-egghead-blog`
+- `npm install`
+- `gatsby develop`
+- visit http://localhost:8000
---
+++ gatsby-config.js
@@ -0,0 +1,61 @@
+module.exports = {
+  pathPrefix: '/',
+  siteMetadata: {
+    siteUrl: 'https://www.frankcalise.com/',
+    author: 'Frank Cailse',
+    title: 'The personal website of Frank Calise',
+    description: 'Thoughts on becoming a better software engineer and bodybuilder.',
+    keywords: [
+      'Software Engineer',
+      'Bodybuilder',
+    ],
+  },
+  plugins: [
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: `${__dirname}/content/blog`,
+        name: 'blog',
+      },
+    },
+    {
+      resolve: `gatsby-mdx`,
+      options: {
+        extensions: ['.mdx', '.md'],
+        gatsbyRemarkPlugins: [
+          {
+            resolve: 'gatsby-remark-images',
+            options: {
+              maxWidth: 1035,
+              sizeByPixelDensity: true,
+            },
+          },
+          {
+            resolve: `gatsby-remark-prismjs`,
+            options: {
+              classPrefix: 'language-',
+              inlineCodeMarker: null,
+              aliases: {},
+            },
+          },
+        ],
+      },
+    },
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-catch-links',
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: 'The personal website of Frank Calise',
+        short_name: 'frankcalise',
+        start_url: '/',
+        background_color: '#fff',
+        theme_color: '#525dce',
+        display: 'standalone',
+      },
+    },
+    'gatsby-plugin-offline',
+  ],
+};
