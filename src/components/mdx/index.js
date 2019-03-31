6aace8871bb0f8454220831579c99d5e8e6995f5
Frank Calise <fcalise@gmail.com>
2019-03-31 11:28:16 -0400

    WIP trying to change code display

--- src/components/mdx/index.js
+++ src/components/mdx/index.js
@@ -1,11 +1,47 @@
-import React from 'react';
+import React from "react"
 
-import Title from './Title';
-import Subtitle from './Subtitle';
-import Paragraph from './Paragraph';
+import Title from "./Title"
+import Subtitle from "./Subtitle"
+import Paragraph from "./Paragraph"
+import Code from "./Code"
 
 export default {
   h1: props => <Title {...props} />,
   h2: props => <Subtitle {...props} />,
   p: props => <Paragraph {...props} />,
-};
+  // pre: preProps => {
+  //   const props = preToCodeBlock(preProps)
+  //   // if there's a codeString and some props, we passed the test
+  //   if (props) {
+  //     return <Code {...props} />
+  //   } else {
+  //     // it's possible to have a pre without a code in it
+  //     return <pre {...preProps} />
+  //   }
+  // },
+}
+
+// lifted this from mdx-utils
+// it doesn't compile it's code and this busted IE, so I'm just vendoring it.
+function preToCodeBlock(preProps) {
+  if (
+    // children is MDXTag
+    preProps.children &&
+    // MDXTag props
+    preProps.children.props &&
+    // if MDXTag is going to render a <code>
+    preProps.children.props.name === "code"
+  ) {
+    // we have a <pre><code> situation
+    const {
+      children: codeString,
+      props: { className, ...props },
+    } = preProps.children.props
+
+    return {
+      codeString: codeString.trim(),
+      language: className && className.split("-")[1],
+      ...props,
+    }
+  }
+}
