0eddae26ff1db70d054d1d7596585b498a32668f
Frank Calise <fcalise@gmail.com>
2019-03-31 08:57:54 -0400

    Added google analytics plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,30 +1,28 @@
 module.exports = {
-  pathPrefix: '/',
+  pathPrefix: "/",
   siteMetadata: {
-    siteUrl: 'https://www.frankcalise.com/',
-    author: 'Frank Cailse',
-    title: 'The personal website of Frank Calise',
-    description: 'Thoughts on becoming a better software engineer and bodybuilder.',
-    keywords: [
-      'Software Engineer',
-      'Bodybuilder',
-    ],
+    siteUrl: "https://www.frankcalise.com/",
+    author: "Frank Cailse",
+    title: "The personal website of Frank Calise",
+    description:
+      "Thoughts on becoming a better software engineer and bodybuilder.",
+    keywords: ["Software Engineer", "Bodybuilder"],
   },
   plugins: [
     {
-      resolve: 'gatsby-source-filesystem',
+      resolve: "gatsby-source-filesystem",
       options: {
         path: `${__dirname}/content/blog`,
-        name: 'blog',
+        name: "blog",
       },
     },
     {
       resolve: `gatsby-mdx`,
       options: {
-        extensions: ['.mdx', '.md'],
+        extensions: [".mdx", ".md"],
         gatsbyRemarkPlugins: [
           {
-            resolve: 'gatsby-remark-images',
+            resolve: "gatsby-remark-images",
             options: {
               maxWidth: 1035,
               sizeByPixelDensity: true,
@@ -33,7 +31,7 @@ module.exports = {
           {
             resolve: `gatsby-remark-prismjs`,
             options: {
-              classPrefix: 'language-',
+              classPrefix: "language-",
               inlineCodeMarker: null,
               aliases: {},
             },
@@ -41,21 +39,27 @@ module.exports = {
         ],
       },
     },
-    'gatsby-plugin-sharp',
-    'gatsby-transformer-sharp',
-    'gatsby-plugin-catch-links',
-    'gatsby-plugin-react-helmet',
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-catch-links",
+    "gatsby-plugin-react-helmet",
     {
-      resolve: 'gatsby-plugin-manifest',
+      resolve: "gatsby-plugin-manifest",
       options: {
-        name: 'The personal website of Frank Calise',
-        short_name: 'frankcalise',
-        start_url: '/',
-        background_color: '#fff',
-        theme_color: '#525dce',
-        display: 'standalone',
+        name: "The personal website of Frank Calise",
+        short_name: "frankcalise",
+        start_url: "/",
+        background_color: "#fff",
+        theme_color: "#525dce",
+        display: "standalone",
       },
     },
-    'gatsby-plugin-offline',
+    {
+      resolve: "gatsby-plugin-google-analytics",
+      options: {
+        trackingId: "UA-15936568-1",
+      },
+    },
+    "gatsby-plugin-offline",
   ],
-};
+}
