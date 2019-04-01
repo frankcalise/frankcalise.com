c63ab066d318bdf5cc11e0c44daf4ecc8d649ace
Frank Calise <fcalise@gmail.com>
2019-03-31 22:50:43 -0400

    Slug for base pages

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,36 +1,36 @@
-const path = require('path');
-const componentWithMDXScope = require('gatsby-mdx/component-with-mdx-scope');
+const path = require("path")
+const createFilePath = require("gatsby-source-filesystem")
 
-const PAGINATION_OFFSET = 2;
+const PAGINATION_OFFSET = 2
 
 const pluckCategories = edges =>
   Object.keys(
     edges.reduce((acc, value) => {
       value.node.fields.categories.forEach(category => {
         if (!acc[category]) {
-          acc[category] = category;
+          acc[category] = category
         }
-      });
+      })
 
-      return acc;
+      return acc
     }, {}),
-  );
+  )
 
 const groupByCategory = edges =>
   edges.reduce((acc, value) => {
     value.node.fields.categories.forEach(category => {
       if (!acc[category]) {
-        acc[category] = [];
+        acc[category] = []
       }
-      acc[category].push(value);
-    });
-    return acc;
-  }, {});
+      acc[category].push(value)
+    })
+    return acc
+  }, {})
 
 const createCategoryPages = (createPage, edges) => {
-  const categories = pluckCategories(edges);
+  const categories = pluckCategories(edges)
 
-  const posts = groupByCategory(edges);
+  const posts = groupByCategory(edges)
 
   Object.keys(posts).forEach(category => {
     createPaginatedPages(
@@ -38,59 +38,49 @@ const createCategoryPages = (createPage, edges) => {
       posts[category],
       `/categories/${category}`,
       { categories, activeCategory: category },
-    );
-  });
-};
+    )
+  })
+}
 
 const createPosts = (createPage, edges) => {
   edges.forEach(({ node }, i) => {
-    const prev = i === 0 ? null : edges[i - 1].node;
-    const next = i === edges.length - 1 ? null : edges[i + 1].node;
+    const prev = i === 0 ? null : edges[i - 1].node
+    const next = i === edges.length - 1 ? null : edges[i + 1].node
 
     createPage({
       path: node.fields.slug,
-      component: componentWithMDXScope(
-        path.resolve(`./src/templates/post.js`),
-        node.code.scope,
-        __dirname,
-      ),
+      component: path.resolve(`./src/templates/post.js`),
       context: {
         id: node.id,
         prev,
         next,
       },
-    });
-  });
-};
+    })
+  })
+}
 
 const createBlog = (createPage, edges) => {
-  const categories = pluckCategories(edges);
+  const categories = pluckCategories(edges)
 
-  createPaginatedPages(createPage, edges, '/blog', { categories });
-};
+  createPaginatedPages(createPage, edges, "/blog", { categories })
+}
 
-const createPaginatedPages = (
-  createPage,
-  edges,
-  pathPrefix,
-  context,
-) => {
+const createPaginatedPages = (createPage, edges, pathPrefix, context) => {
   const pages = edges.reduce((acc, value, index) => {
-    const pageIndex = Math.floor(index / PAGINATION_OFFSET);
+    const pageIndex = Math.floor(index / PAGINATION_OFFSET)
 
     if (!acc[pageIndex]) {
-      acc[pageIndex] = [];
+      acc[pageIndex] = []
     }
 
-    acc[pageIndex].push(value.node.id);
+    acc[pageIndex].push(value.node.id)
 
-    return acc;
-  }, []);
+    return acc
+  }, [])
 
   pages.forEach((page, index) => {
-    const previousPagePath = `${pathPrefix}/${index + 1}`;
-    const nextPagePath =
-      index === 1 ? pathPrefix : `${pathPrefix}/${index - 1}`;
+    const previousPagePath = `${pathPrefix}/${index + 1}`
+    const nextPagePath = index === 1 ? pathPrefix : `${pathPrefix}/${index - 1}`
 
     createPage({
       path: index > 0 ? `${pathPrefix}/${index}` : `${pathPrefix}`,
@@ -106,9 +96,9 @@ const createPaginatedPages = (
         },
         ...context,
       },
-    });
-  });
-};
+    })
+  })
+}
 
 exports.createPages = ({ actions, graphql }) =>
   graphql(`
@@ -132,79 +122,82 @@ exports.createPages = ({ actions, graphql }) =>
     }
   `).then(({ data, errors }) => {
     if (errors) {
-      return Promise.reject(errors);
+      return Promise.reject(errors)
     }
 
-    const { edges } = data.allMdx;
+    const { edges } = data.allMdx
 
-    createBlog(actions.createPage, edges);
-    createPosts(actions.createPage, edges);
-    createCategoryPages(actions.createPage, edges);
-  });
+    createBlog(actions.createPage, edges)
+    createPosts(actions.createPage, edges)
+    createCategoryPages(actions.createPage, edges)
+  })
 
 exports.onCreateWebpackConfig = ({ actions }) => {
   actions.setWebpackConfig({
     resolve: {
-      modules: [path.resolve(__dirname, 'src'), 'node_modules'],
+      modules: [path.resolve(__dirname, "src"), "node_modules"],
       alias: {
-        $components: path.resolve(__dirname, 'src/components'),
+        $components: path.resolve(__dirname, "src/components"),
       },
     },
-  });
-};
+  })
+}
 
 exports.onCreateNode = ({ node, getNode, actions }) => {
-  const { createNodeField } = actions;
+  const { createNodeField } = actions
 
   if (node.internal.type === `Mdx`) {
-    const parent = getNode(node.parent);
+    const parent = getNode(node.parent)
+    let slug =
+      node.frontmatter.slug ||
+      createFilePath({ node, getNode, basePath: `pages` })
 
     createNodeField({
-      name: 'id',
+      name: "id",
       node,
       value: node.id,
-    });
+    })
 
     createNodeField({
-      name: 'title',
+      name: "title",
       node,
       value: node.frontmatter.title,
-    });
+    })
 
     createNodeField({
-      name: 'description',
+      name: "description",
       node,
       value: node.frontmatter.description,
-    });
+    })
 
     createNodeField({
-      name: 'slug',
+      name: "slug",
       node,
-      value: node.frontmatter.slug,
-    });
+      value: slug,
+    })
 
     createNodeField({
-      name: 'date',
+      name: "date",
       node,
-      value: node.frontmatter.date || '',
-    });
+      value: node.frontmatter.date || "",
+    })
 
     createNodeField({
-      name: 'banner',
+      name: "banner",
       node,
       banner: node.frontmatter.banner,
-    });
+    })
 
     createNodeField({
-      name: 'categories',
+      name: "categories",
       node,
       value: node.frontmatter.categories || [],
-    });
+    })
 
     createNodeField({
-      name: 'keywords',
+      name: "keywords",
       node,
       value: node.frontmatter.keywords || [],
-    });
+    })
   }
-};
+}
