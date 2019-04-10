d544e870123b31f24dbe03eb272c3624a7f90b80
Frank Calise <fcalise@gmail.com>
2019-04-10 10:21:47 -0400

    Use emotion for global style, added theme to match my vscode editor (still WIP)

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,37 +2,21 @@ import React from "react"
 import Helmet from "react-helmet"
 import { graphql, StaticQuery } from "gatsby"
 import { MDXProvider } from "@mdx-js/tag"
-import { createGlobalStyle } from "styled-components"
-import Header from './Header'
-
-import "prismjs/themes/prism-okaidia.css"
-
-// import Link from "./Link"
+import { Global, css } from "@emotion/core"
+import Header from "./Header"
 import mdxComponents from "./mdx"
 
-createGlobalStyle`
-  html, body {
+import "../themes/material-darker-theme.css"
+
+export const globalStyles = css`
+  html,
+  body {
     margin: 0;
     padding: 0;
   }
-
-  ${() => {
-    /* Override PrismJS Defaults */ return null
-  }}
-
   pre {
-    background-color: #2f1e2e !important;
     border-radius: 4px;
     font-size: 14px;
-  }
-
-  .gatsby-highlight-code-line {
-    background-color: #4f424c;
-    display: block;
-    margin-right: -1em;
-    margin-left: -1em;
-    padding-right: 1em;
-    padding-left: 1em;
   }
 `
 
@@ -52,6 +36,7 @@ function Layout({ data, frontmatter = {}, children }) {
 
   return (
     <div style={{ margin: `3rem auto`, maxWidth: 650, padding: `0 1rem` }}>
+      <Global styles={globalStyles} />
       <Helmet
         title={title}
         meta={[
