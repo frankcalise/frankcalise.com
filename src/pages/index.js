aa59db85030c35f5a0d26658fca6ea7fda987754
Frank Calise <fcalise@gmail.com>
2019-05-20 21:01:44 -0400

    Update landing page with a message

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,19 @@
-import React from 'react';
-import Layout from '../components/Layout';
+import React from "react"
+import Layout from "../components/Layout"
+import Link from '../components/Link'
 
 export default function Index() {
-  return <Layout>Landing Page</Layout>;
+  return (
+    <Layout>
+      <p>
+        Hey, welcome! My site is still very much in the works, but the{" "}
+        <Link to="/blog">Blog</Link> and <Link to="/about">About</Link> pages
+        are live.
+      </p>
+      <p>
+        If I was to wait on getting the layout the way I like it, I'd never
+        launch and produce actual content.
+      </p>
+    </Layout>
+  )
 }
